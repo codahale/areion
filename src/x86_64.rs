09104352a065b01f1fe56c1e3ea3da4c6c523e5b
Coda Hale <coda.hale@gmail.com>
2023-11-06 09:30:45 -0700

    fix: fix x86_64 builds

--- src/x86_64.rs
+++ src/x86_64.rs
@@ -1,19 +1,19 @@
 use core::arch::x86_64::*;
 
-pub use core::arch::x86_64::__m128i as Block;
+pub use core::arch::x86_64::__m128i as AesBlock;
 
 #[inline]
-pub fn zero() -> Block {
+pub fn zero() -> AesBlock {
     unsafe { _mm_setzero_si128() }
 }
 
 #[inline]
-pub fn load_64x2(a: u64, b: u64) -> Block {
+pub fn load_64x2(a: u64, b: u64) -> AesBlock {
     unsafe { _mm_set_epi64x(b as i64, a as i64) }
 }
 
 #[inline]
-pub fn load_32x4(a: u32, b: u32, c: u32, d: u32) -> Block {
+pub fn load_32x4(a: u32, b: u32, c: u32, d: u32) -> AesBlock {
     unsafe {
         _mm_set_epi32(
             d.try_into().unwrap(),
@@ -25,41 +25,41 @@ pub fn load_32x4(a: u32, b: u32, c: u32, d: u32) -> Block {
 }
 
 #[inline]
-pub fn load(bytes: &[u8]) -> Block {
+pub fn load(bytes: &[u8]) -> AesBlock {
     unsafe { _mm_loadu_si128(bytes.as_ptr() as *const __m128i) }
 }
 
 #[inline]
-pub fn store(bytes: &mut [u8], block: Block) {
+pub fn store(bytes: &mut [u8], block: AesBlock) {
     unsafe { _mm_storeu_si128(bytes.as_mut_ptr() as *mut __m128i, block) };
 }
 
 #[inline]
-pub fn xor(a: Block, b: Block) -> Block {
+pub fn xor(a: AesBlock, b: AesBlock) -> AesBlock {
     unsafe { _mm_xor_si128(a, b) }
 }
 
 #[inline]
-pub fn xor3(a: Block, b: Block, c: Block) -> Block {
+pub fn xor3(a: AesBlock, b: AesBlock, c: AesBlock) -> AesBlock {
     unsafe { _mm_xor_si128(a, _mm_xor_si128(b, c)) }
 }
 
 #[inline]
-pub fn enc(state: Block, round_key: Block) -> Block {
+pub fn enc(state: AesBlock, round_key: AesBlock) -> AesBlock {
     unsafe { _mm_aesenc_si128(state, round_key) }
 }
 
 #[inline]
-pub fn enc_last(state: Block, round_key: Block) -> Block {
+pub fn enc_last(state: AesBlock, round_key: AesBlock) -> AesBlock {
     unsafe { _mm_aesenclast_si128(state, round_key) }
 }
 
 #[inline]
-pub fn dec_last(state: Block, round_key: Block) -> Block {
+pub fn dec_last(state: AesBlock, round_key: AesBlock) -> AesBlock {
     unsafe { _mm_aesdeclast_si128(state, round_key) }
 }
 
 #[inline]
-pub fn inv_mix(state: Block) -> Block {
+pub fn inv_mix(state: AesBlock) -> AesBlock {
     unsafe { _mm_aesimc_si128(state) }
 }
