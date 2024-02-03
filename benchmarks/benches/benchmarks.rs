102b21ad6d9fdb4a2ff9f56eb5b6014eec7cdd56
Coda Hale <coda.hale@gmail.com>
2024-02-03 16:19:07 -0700

    perf(benchmarks): switch to criterion

--- benchmarks/benches/benchmarks.rs
+++ benchmarks/benches/benchmarks.rs
@@ -1,105 +1,161 @@
-use areion::digest::Digest;
+use areion::{digest::Digest, Areion512Md, Areion512Mmo};
 use areion::{Areion256Sponge, AreionHaifa512};
-use divan::counter::BytesCount;
-use divan::Bencher;
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion, Throughput};
 use sha2::{Sha256, Sha512};
 
-#[divan::bench(counters = [BytesCount::new(32usize)])]
-fn areion256(b: Bencher) {
-    b.with_inputs(|| (areion::load(&[0u8; 16]), areion::load(&[0u8; 16])))
-        .bench_values(|(x0, x1)| areion::areion256(x0, x1))
+fn areion256(c: &mut Criterion) {
+    c.bench_function("areion256", |b| {
+        b.iter_batched(
+            || (areion::load(&[0u8; 16]), areion::load(&[0u8; 16])),
+            |(x0, x1)| areion::areion256(x0, x1),
+            BatchSize::SmallInput,
+        );
+    });
 }
 
-#[divan::bench(counters = [BytesCount::new(64usize)])]
-fn areion512(b: Bencher) {
-    b.with_inputs(|| {
-        (
-            areion::load(&[0u8; 16]),
-            areion::load(&[0u8; 16]),
-            areion::load(&[0u8; 16]),
-            areion::load(&[0u8; 16]),
-        )
-    })
-    .bench_values(|(x0, x1, x2, x3)| areion::areion512(x0, x1, x2, x3))
+fn areion512(c: &mut Criterion) {
+    c.bench_function("areion512", |b| {
+        b.iter_batched(
+            || {
+                (
+                    areion::load(&[0u8; 16]),
+                    areion::load(&[0u8; 16]),
+                    areion::load(&[0u8; 16]),
+                    areion::load(&[0u8; 16]),
+                )
+            },
+            |(x0, x1, x2, x3)| areion::areion512(x0, x1, x2, x3),
+            BatchSize::SmallInput,
+        );
+    });
 }
 
-#[divan::bench(counters = [BytesCount::new(32usize)])]
-fn areion256_dm(b: Bencher) {
-    b.with_inputs(|| (areion::load(&[0u8; 16]), areion::load(&[0u8; 16])))
-        .bench_values(|(x0, x1)| areion::areion256_dm(x0, x1))
+fn areion256_dm(c: &mut Criterion) {
+    c.bench_function("areion256_dm", |b| {
+        b.iter_batched(
+            || (areion::load(&[0u8; 16]), areion::load(&[0u8; 16])),
+            |(x0, x1)| areion::areion256_dm(x0, x1),
+            BatchSize::SmallInput,
+        );
+    });
 }
 
-#[divan::bench(counters = [BytesCount::new(64usize)])]
-fn areion512_dm(b: Bencher) {
-    b.with_inputs(|| {
-        (
-            areion::load(&[0u8; 16]),
-            areion::load(&[0u8; 16]),
-            areion::load(&[0u8; 16]),
-            areion::load(&[0u8; 16]),
-        )
-    })
-    .bench_values(|(x0, x1, x2, x3)| areion::areion512_dm(x0, x1, x2, x3))
+fn areion512_dm(c: &mut Criterion) {
+    c.bench_function("areion512_dm", |b| {
+        b.iter_batched(
+            || {
+                (
+                    areion::load(&[0u8; 16]),
+                    areion::load(&[0u8; 16]),
+                    areion::load(&[0u8; 16]),
+                    areion::load(&[0u8; 16]),
+                )
+            },
+            |(x0, x1, x2, x3)| areion::areion512_dm(x0, x1, x2, x3),
+            BatchSize::SmallInput,
+        );
+    });
 }
 
-const LENS: &[usize] = &[16, 256, 1024, 16 * 1024, 1024 * 1024];
+const LENS: &[(usize, &str)] =
+    &[(16, "16B"), (256, "256B"), (1024, "1KiB"), (16 * 1024, "16KiB"), (1024 * 1024, "1MiB")];
 
-#[divan::bench(args = LENS)]
-fn areion512_md(bencher: divan::Bencher, len: usize) {
-    bencher
-        .with_inputs(|| vec![0u8; len])
-        .counter(BytesCount::new(len))
-        .bench_refs(|block| areion::Areion512Md::default().chain_update(block).finalize());
+fn areion512_md(c: &mut Criterion) {
+    let mut g = c.benchmark_group("areion512_md");
+    for &(len, id) in LENS {
+        let input = vec![0u8; len];
+        g.throughput(Throughput::Bytes(len as u64));
+        g.bench_with_input(id, &input, |b, block| {
+            b.iter(|| Areion512Md::default().chain_update(block).finalize());
+        });
+    }
+    g.finish();
 }
 
-#[divan::bench(args = LENS)]
-fn areion512_mmo(bencher: divan::Bencher, len: usize) {
-    bencher
-        .with_inputs(|| vec![0u8; len])
-        .counter(BytesCount::new(len))
-        .bench_refs(|block| areion::Areion512Mmo::default().chain_update(block).finalize());
+fn areion512_mmo(c: &mut Criterion) {
+    let mut g = c.benchmark_group("areion512_mmo");
+    for &(len, id) in LENS {
+        let input = vec![0u8; len];
+        g.throughput(Throughput::Bytes(len as u64));
+        g.bench_with_input(id, &input, |b, block| {
+            b.iter(|| Areion512Mmo::default().chain_update(block).finalize());
+        });
+    }
+    g.finish();
 }
 
-#[divan::bench(args = LENS)]
-fn areion256_512_sponge(bencher: divan::Bencher, len: usize) {
-    bencher
-        .with_inputs(|| vec![0u8; len])
-        .counter(BytesCount::new(len))
-        .bench_refs(|block| Areion256Sponge::new().chain_update(block).finalize());
+fn areion256_sponge(c: &mut Criterion) {
+    let mut g = c.benchmark_group("areion256_sponge");
+    for &(len, id) in LENS {
+        let input = vec![0u8; len];
+        g.throughput(Throughput::Bytes(len as u64));
+        g.bench_with_input(id, &input, |b, block| {
+            b.iter(|| Areion256Sponge::default().chain_update(block).finalize());
+        });
+    }
+    g.finish();
 }
 
-#[divan::bench(args = LENS)]
-fn areion512_haifa(bencher: divan::Bencher, len: usize) {
-    bencher
-        .with_inputs(|| vec![0u8; len])
-        .counter(BytesCount::new(len))
-        .bench_refs(|block| AreionHaifa512::new().chain_update(block).finalize());
+fn areion512_haifa(c: &mut Criterion) {
+    let mut g = c.benchmark_group("areion512_haifa");
+    for &(len, id) in LENS {
+        let input = vec![0u8; len];
+        g.throughput(Throughput::Bytes(len as u64));
+        g.bench_with_input(id, &input, |b, block| {
+            b.iter(|| AreionHaifa512::default().chain_update(block).finalize());
+        });
+    }
+    g.finish();
 }
 
-#[divan::bench(args = LENS)]
-fn sha256(bencher: divan::Bencher, len: usize) {
-    bencher
-        .with_inputs(|| vec![0u8; len])
-        .counter(BytesCount::new(len))
-        .bench_refs(|block| Sha256::new().chain_update(block).finalize());
+fn sha256(c: &mut Criterion) {
+    let mut g = c.benchmark_group("sha256");
+    for &(len, id) in LENS {
+        let input = vec![0u8; len];
+        g.throughput(Throughput::Bytes(len as u64));
+        g.bench_with_input(id, &input, |b, block| {
+            b.iter(|| Sha256::default().chain_update(block).finalize());
+        });
+    }
+    g.finish();
 }
 
-#[divan::bench(args = LENS)]
-fn blake3(bencher: divan::Bencher, len: usize) {
-    bencher
-        .with_inputs(|| vec![0u8; len])
-        .counter(BytesCount::new(len))
-        .bench_refs(|block| blake3::hash(block));
+fn sha512(c: &mut Criterion) {
+    let mut g = c.benchmark_group("sha512");
+    for &(len, id) in LENS {
+        let input = vec![0u8; len];
+        g.throughput(Throughput::Bytes(len as u64));
+        g.bench_with_input(id, &input, |b, block| {
+            b.iter(|| Sha512::default().chain_update(block).finalize());
+        });
+    }
+    g.finish();
 }
 
-#[divan::bench(args = LENS)]
-fn sha512(bencher: divan::Bencher, len: usize) {
-    bencher
-        .with_inputs(|| vec![0u8; len])
-        .counter(BytesCount::new(len))
-        .bench_refs(|block| Sha512::new().chain_update(block).finalize());
+fn blake3(c: &mut Criterion) {
+    let mut g = c.benchmark_group("blake3");
+    for &(len, id) in LENS {
+        let input = vec![0u8; len];
+        g.throughput(Throughput::Bytes(len as u64));
+        g.bench_with_input(id, &input, |b, block| {
+            b.iter(|| ::blake3::hash(block));
+        });
+    }
+    g.finish();
 }
 
-fn main() {
-    divan::main();
-}
+criterion_group!(
+    benches,
+    areion256,
+    areion512,
+    areion256_dm,
+    areion512_dm,
+    areion512_md,
+    areion512_mmo,
+    areion256_sponge,
+    areion512_haifa,
+    sha256,
+    sha512,
+    blake3,
+);
+criterion_main!(benches);
